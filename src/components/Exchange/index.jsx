ed94fafd35b4af5f6916fcc1bdf39681dcd0895d
paupe2 <pau.penya.trevinyo@gmail.com>
2023-01-12 12:50:13 +0100

    exchange component

---
+++ src/components/Exchange/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exchange = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Exchange
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css'
 import { ThemeProvider } from '@emotion/react'
 import { easyChainTheme } from './styles/easyChainTheme'
 import App from './components/App/App'
+import Exchange from './components/Exchange/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={easyChainTheme}>
       <App />
-    </ThemeProvider>
+      <Exchange />
+   </ThemeProvider>
   </React.StrictMode>
 )
