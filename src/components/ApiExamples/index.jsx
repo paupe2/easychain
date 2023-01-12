ad8a4de08c09a6224c2415f4c185a816bb6e9f39
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-12 12:57:24 +0100

    added useNft

--- src/components/ApiExamples/index.jsx
+++ src/components/ApiExamples/index.jsx
@@ -1,16 +1,37 @@
 import { useNFTPort } from '../ApiCaller'
+import { useNft } from 'use-nft'
+
+const NftDisplayer = ({ nftData = { contract_address: '', token_id: '' } }) => {
+  const { loading, nft } = useNft(
+    nftData.contract_address,
+    nftData.token_id
+  )
+  return (
+    <>
+      {loading
+        ? <p>
+          <b>Chain:</b> {nft.chain}
+          <b>Contract Address:</b> {nft.contract_address}
+          <b>Creator Address:</b> {nft.creator_address}
+          <b>Token Id:</b> {nft.token_id}
+        </p>
+        : <>
+          <h1>{nft.name}</h1>
+          <img src={nft.image} alt='' />
+          <p>{nft.description}</p>
+          <p>Owner: {nft.owner}</p>
+          <p>Metadata URL: {nft.metadataUrl}</p>
+        </>}
+    </>
+  )
+}
 
 const NftsDisplayer = ({ nfts = [] }) => {
   return (
     <ul>
       {nfts.map((nft, index) =>
         <li key={index}>
-          <p>
-            <b>Chain:</b> {nft.chain}
-            <b>Contract Address:</b> {nft.contract_address}
-            <b>Creator Address:</b> {nft.creator_address}
-            <b>Token Id:</b> {nft.token_id}
-          </p>
+          <NftDisplayer nftData={nft} />
         </li>)}
     </ul>
   )
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
 import { easyChainTheme } from './styles/easyChainTheme'
+import { getDefaultProvider } from 'ethers'
+import { NftProvider } from 'use-nft'
 import App from './components/App'
+
+const ethersConfig = {
+  provider: getDefaultProvider('homestead')
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={easyChainTheme}>
-      <App />
+      <NftProvider fetcher={['ethers', ethersConfig]}>
+        <App />
+      </NftProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
