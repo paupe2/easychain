f05c60a0b0cd50292b1d09c4250de09e7449aad2
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-13 11:07:16 +0100

    oops.js

--- src/components/ApiCaller/index.js
+++ src/components/ApiCaller/index.js
@@ -4,11 +4,6 @@ import { useState, useEffect } from 'react'
 export const useNFTPort = ({ chain = 'ethereum' }) => {
   const [responseState, setResponseState] = useState({ response: {}, isResolved: false })
 
-  /* const fetchData = async (config) => {
-    const response = await axios.get('https://api.nftport.xyz/v0/nfts?chain=' + chain + '&include=all', config)
-    setResponseState({ response, isResolved: true })
-  } */
-
   useEffect(() => {
     const config = {
       headers: {
@@ -16,7 +11,7 @@ export const useNFTPort = ({ chain = 'ethereum' }) => {
         Authorization: '89959bd1-d013-40bd-9a4a-5f435a78eb86'
       }
     }
-    // fetchData(config)
+
     const fecthData = async () => {
       const response = await axios.get('https://api.nftport.xyz/v0/nfts?chain=' + chain + '&include=all', config)
       setResponseState({ response, isResolved: true })
