5cce17a20ed0262093b7975d6f754091b1bdd98e
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-16 11:28:14 +0100

    .

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,13 @@
 import { HighlightTitle } from './styles'
+import FilterInput from '../FilterInput'
+import LoadingComponent from '../LoadingComponent'
 
 function App () {
   return (
     <div className='App'>
       <HighlightTitle>
         Hello
-      </HighlightTitle>  
+      </HighlightTitle>
       <FilterInput options={['One', 'Two', 'Three', 'Caramba']} onChange={({ prop = {} }) => { console.log(prop) }} />
       <div style={{ width: '300px', height: '300px' }}>
         <LoadingComponent />
