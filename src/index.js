16d1dc5956c4902bf61da02f18a6403c7c7ed017
paupe2 <pau.penya.trevinyo@gmail.com>
2023-01-20 03:59:07 +0100

    app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
 import { easyChainTheme } from './styles/easyChainTheme'
-import App from './components/App/App'
+import App from './components/App/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
