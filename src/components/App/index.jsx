ea7dd614b8c0d0448dcf9bfe4b5455d0f47f5b5a
paupe2 <pau.penya.trevinyo@gmail.com>
2023-01-20 04:39:51 +0100

    update

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../SideBar/Navbar'
 import Home from '../SideBar/pages/Home'
 import Crypto from '../SideBar/pages/Crypto'
-import NFTS from '../SideBar/pages/Nfts'
+import Nfts from '../SideBar/pages/Nfts';
 import Exchanges from '../SideBar/pages/Exchanges'
 import Support from '../SideBar/pages/Support';
+
 
 
 function App () {
@@ -15,8 +16,8 @@ function App () {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/nfts' element={<NFTS />} />
-          <Route path='/crypto' element={<Crypto />} />
+          <Route path='/nfts' element={<Nfts />} />
+          <Route path='/cryptos' element={<Crypto />} />
           <Route path='/exchanges' element={<Exchanges />} />
           <Route path='/support' element={<Support />} />
         </Routes>
